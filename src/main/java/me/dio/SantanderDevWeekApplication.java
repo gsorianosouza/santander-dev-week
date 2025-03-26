6f4e392ba493c3be8593500a0d8b639e1a0471af
gsorianosouza <gabrielsorianosouza@outlook.com.br>
2025-03-26 11:50:56 -0300

    Finalizando

--- src/main/java/me/dio/SantanderDevWeekApplication.java
+++ src/main/java/me/dio/SantanderDevWeekApplication.java
@@ -1,13 +1,16 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-public class SantanderDevWeekApplication {
+public class SantanderDevWeek2023Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeekApplication.class, args);
+		SpringApplication.run(SantanderDevWeek2023Application.class, args);
 	}
 
 }
